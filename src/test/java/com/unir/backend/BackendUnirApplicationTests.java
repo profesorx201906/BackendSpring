e5298eb7b463162ea3e37b13a56d6a30189a422e
Rafael Suarez <rafasuarez0202@gmail.com>
2024-04-26 11:08:47 -0500

    Creación de proyecto

---
+++ src/test/java/com/unir/backend/BackendUnirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUnirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
