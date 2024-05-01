68906600785c1b52e13fb6adb5851bc0482d4084
Javier Díaz Díaz <jddiazsena@gmail.com>
2024-05-01 17:42:05 -0500

    Creación de mappers de categoria y producto

---
+++ src/main/java/com/unir/backend/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,23 @@
+package com.unir.backend.persistence.mapper;
+
+import com.unir.backend.domain.dto.CategoryDTO;
+import com.unir.backend.persistence.entity.CategoriaEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active"),
+    })
+    CategoryDTO toCategory(CategoriaEntity categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    CategoriaEntity toCategoria(CategoryDTO category);
+}
