bff4a969da6c1c4ca7251a748eaef1b9a6ad4cb5
Javier Díaz Díaz <jddiazsena@gmail.com>
2024-06-03 18:41:55 -0500

    Repositorio de dominio

---
+++ src/main/java/com/unir/backend/domain/repository/PurchaseDTORepository.java
@@ -0,0 +1,11 @@
+package com.unir.backend.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+import com.unir.backend.domain.dto.Purchase;
+
+public interface PurchaseDTORepository {
+List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
